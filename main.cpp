2b600b5ef8f2c421e2b2b1be30a89e1c911f3c0d
Henryawd345 <henrygoodboy2000@gmail.com>
2025-11-14 03:34:36 -0800

    update

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 #include <deque>
+#include <cstdlib>
+#include <ctime>
+#include "Car.h"
+
+using namespace std;
+
+int INITIAL_SIZE = 2;
+
+void printQueue(const deque<Car> &lane){
+    cout << "Queue: \n";
+    if (lane.empty()){
+        cout << "   Empty\n";
+        return;
+    }
+
+    for (const Car &c : lane){
+        cout << "    ";
+        c.print();
+    }
+
+}
 
 int main() {
     
